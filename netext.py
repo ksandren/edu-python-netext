9c768742e07560b45f62c4fa659ce838958dbdc9
ksandren <ksandren@mail.ru>
2022-02-14 23:48:18 +0700

    Begin to analyze user input

--- netext.py
+++ netext.py
@@ -3,7 +3,10 @@
 import random
 from tkinter import *
 import wsmgr
-import locale
+
+IDENT = 5
+LINE_LEN = 60
+LINE_HEIGHT = 20
 
 
 class StringGenerator:
@@ -28,15 +31,37 @@ class StringGenerator:
 class TextState:
     def __init__(self):
         str_gen = StringGenerator('RU')
-        self.current_line = str_gen.gen_rand_string(80)
-        self.next_line = str_gen.gen_rand_string(80)
+        self.current_line = str_gen.gen_rand_string(LINE_LEN)
+        self.next_line = str_gen.gen_rand_string(LINE_LEN)
         self.current_input = ''
+        self.current_highlight = '_' * (len(self.current_line) + 1)
 
     def key_pressed(self, event):
         if event.keysym == 'BackSpace':
             if len(self.current_input) > 0:
                 self.current_input = self.current_input[:-1]
+                ci_len = len(self.current_input)
+                self.current_highlight =\
+                    self.current_highlight[:ci_len] +\
+                    'x' + self.current_highlight[ci_len + 1:]
+
         elif event.char is not None and event.char.isprintable():
+            ci_len = len(self.current_input)
+            if ci_len >= len(self.current_line):
+                return
+            if self.current_line[ci_len] == event.char:
+                if self.current_highlight[ci_len] == '_':
+                    self.current_highlight =\
+                        self.current_highlight[:ci_len] +\
+                        '+' + self.current_highlight[ci_len + 1:]
+                else:
+                    self.current_highlight =\
+                        self.current_highlight[:ci_len] +\
+                        'v' + self.current_highlight[ci_len + 1:]
+            else:
+                self.current_highlight =\
+                    self.current_highlight[:ci_len] +\
+                    '-' + self.current_highlight[ci_len + 1:]
             self.current_input += event.char
 
 
@@ -57,12 +82,29 @@ class MainWindow:
 
     def update(self):
         self.cvs.delete('all')
-        self.cvs.create_text(400, 100,
+        self.cvs.create_text(IDENT, IDENT,
+                             anchor='nw',
                              text=self.text_state.next_line, font='Consolas')
-        self.cvs.create_text(400, 120,
-                             text=self.text_state.current_line, font='Consolas')
-        self.cvs.create_text(400, 140,
-                             text=self.text_state.current_input, font='Consolas')
+        last_x = IDENT
+        for i in range(len(self.text_state.current_line)):
+            if len(self.text_state.current_input) > i:
+                new_char = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
+                                                anchor='nw',
+                                                text=self.text_state.current_input[i],
+                                                font='Consolas')
+                x1, y1, x2, y2 = self.cvs.bbox(new_char)
+                self.cvs.create_line(x1, y2 + 1, x2, y2 + 1)
+            else:
+                new_char = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
+                                                anchor='nw',
+                                                text=self.text_state.current_line[i],
+                                                font='Consolas')
+                x1, y1, x2, y2 = self.cvs.bbox(new_char)
+            last_x = x2 - 1
+
+        self.cvs.create_text(IDENT, IDENT + 2*LINE_HEIGHT,
+                             anchor='nw',
+                             text=self.text_state.current_highlight, font='Consolas')
 
     def cvs_key_event(self, event):
         self.text_state.key_pressed(event)
@@ -70,7 +112,6 @@ class MainWindow:
 
 
 def main():
-    locale.setlocale(locale.LC_ALL, 'ru_RU.UTF8')
     main_window = MainWindow()
     main_window.window.mainloop()
 
