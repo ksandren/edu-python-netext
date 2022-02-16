bcb2ecfbb19c3938ddb4c8acdf00456f194773f4
ksandren <ksandren@mail.ru>
2022-02-16 22:08:28 +0700

    Add cursor movement

--- netext.py
+++ netext.py
@@ -33,35 +33,51 @@ class TextState:
         self._str_gen = StringGenerator('RU')
         self.current_line = self._str_gen.gen_rand_string(LINE_LEN)
         self.next_line = self._str_gen.gen_rand_string(LINE_LEN)
-        self.current_input = ''
+        self.current_input = [''] * len(self.current_line)
         self.current_highlight = ['_'] * len(self.current_line)
         self.prev_line = ''
+        self.cursor_pos = 0
+        self.input_len = 0
 
     def drop_char(self):
-        if len(self.current_input) > 0:
-            self.current_input = self.current_input[:-1]
-            ci_len = len(self.current_input)
-            self.current_highlight[ci_len] = 'x'
+        if self.cursor_pos > 0:
+            if self.input_len == self.cursor_pos:
+                self.input_len -= 1
+            self.cursor_pos -= 1
+            self.current_input[self.cursor_pos] = ''
+            self.current_highlight[self.cursor_pos] = 'x'
 
     def put_char(self, char):
-        ci_len = len(self.current_input)
-        if ci_len >= len(self.current_line):
+        if self.cursor_pos >= len(self.current_line):
             return
-        if self.current_line[ci_len] == char:
-            if self.current_highlight[ci_len] == '_':
-                self.current_highlight[ci_len] = '+'
+        if self.current_line[self.cursor_pos] == char:
+            if self.current_highlight[self.cursor_pos] == '_':
+                self.current_highlight[self.cursor_pos] = '+'
             else:
-                self.current_highlight[ci_len] = 'v'
+                self.current_highlight[self.cursor_pos] = 'v'
         else:
-            self.current_highlight[ci_len] = '-'
-        self.current_input += char
+            self.current_highlight[self.cursor_pos] = '-'
+        self.current_input[self.cursor_pos] = char
+        self.cursor_pos += 1
+        if self.input_len < self.cursor_pos:
+            self.input_len = self.cursor_pos
 
     def new_line(self):
         self.prev_line = self.current_line
         self.current_line = self.next_line
         self.next_line = self._str_gen.gen_rand_string(LINE_LEN)
         self.current_highlight = ['_'] * len(self.current_line)
-        self.current_input = ''
+        self.current_input = [''] * len(self.current_line)
+        self.cursor_pos = 0
+        self.input_len = 0
+
+    def move_left(self):
+        if self.cursor_pos > 0:
+            self.cursor_pos -= 1
+
+    def move_right(self):
+        if self.cursor_pos < self.input_len:
+            self.cursor_pos += 1
 
 
 class MainWindow:
@@ -89,28 +105,22 @@ class MainWindow:
         # Draw current line with current input and highlight
         last_x = IDENT
         for i in range(len(self.text_state.current_line)):
-            if len(self.text_state.current_input) > i:
-                # Draw symbols from input
-                new_char = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
-                                                anchor='nw',
-                                                text=self.text_state.current_input[i],
-                                                font='Consolas')
-                x1, y1, x2, y2 = self.cvs.bbox(new_char)
-            else:
-                # Draw symbols from generated text
-                new_char = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
-                                                anchor='nw',
-                                                text=self.text_state.current_line[i],
-                                                font='Consolas', fill='gray')
-                x1, y1, x2, y2 = self.cvs.bbox(new_char)
-                # Draw cursor
-                if i == len(self.text_state.current_input):
-                    self.cvs.create_line(x1 + 1, y1 + 1, x1 + 1, y2 - 1, width=2)
+            new_char = self.text_state.current_input[i]
+            color = 'black'
+            if new_char == '':
+                new_char = self.text_state.current_line[i]
+                color = 'gray'
+            new_char_id = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
+                                               anchor='nw',
+                                               text=new_char,
+                                               font='Consolas', fill=color)
+            x1, y1, x2, y2 = self.cvs.bbox(new_char_id)
+            if self.text_state.cursor_pos == i:
+                self.cvs.create_line(x1 + 1, y1 + 1, x1 + 1, y2 - 1, width=2)
             # Draw highlight
             highlight = self.text_state.current_highlight[i]
             if highlight == '+':  # Correct
                 self.cvs.create_line(x1, y2, x2, y2, fill='lightgreen', width='2p')
-                pass
             elif highlight == '-':  # Incorrect
                 self.cvs.create_rectangle(x1 + 1, y1 + 1, x2 - 1, y2 - 1, outline='', fill='red', width='2p')
                 self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
@@ -118,13 +128,10 @@ class MainWindow:
                                      text=self.text_state.current_input[i],
                                      font='Consolas',
                                      fill='gray')
-                pass
             elif highlight == 'x':  # Deleted
                 self.cvs.create_line(x1, y2, x2, y2, fill='red', width='2p')
-                pass
             elif highlight == 'v':  # Fixed
                 self.cvs.create_line(x1, y2, x2, y2, fill='orange', width='2p')
-                pass
             last_x = x2 - 1
         # Draw prev line
         self.cvs.create_text(IDENT, IDENT + 2*LINE_HEIGHT,
@@ -135,8 +142,13 @@ class MainWindow:
         if event.keysym == 'BackSpace':
             self.text_state.drop_char()
         elif event.keysym == 'Return':
-            if self.text_state.current_input == self.text_state.current_line:
+            if ''.join(self.text_state.current_input) ==\
+                    self.text_state.current_line:
                 self.text_state.new_line()
+        elif event.keysym == 'Left':
+            self.text_state.move_left()
+        elif event.keysym == 'Right':
+            self.text_state.move_right()
         elif event.char is not None and \
                 event.char != '' and event.char.isprintable():
             self.text_state.put_char(event.char)
