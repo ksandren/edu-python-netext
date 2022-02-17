3842cc5dcb7172e52bf1595c9e4446fd7e60144c
ksandren <ksandren@mail.ru>
2022-02-17 23:15:53 +0700

    Add hint, print statistics, try two methods to count speed

--- netext.py
+++ netext.py
@@ -7,6 +7,7 @@ import wsmgr
 IDENT = 5
 LINE_LEN = 40
 LINE_HEIGHT = 20
+FONT = ('Consolas', 14)
 
 
 class StringGenerator:
@@ -38,8 +39,14 @@ class TextState:
         self.prev_line = ''
         self.cursor_pos = 0
         self.input_len = 0
+        self.useful_keys = 0
+        self.useless_keys = 0
+        self.input_speed = 0
+        self.prev_count = 0
+        self.cycle_count = 0
 
     def drop_char(self):
+        self.useless_keys += 1
         if self.cursor_pos > 0:
             if self.input_len == self.cursor_pos:
                 self.input_len -= 1
@@ -49,35 +56,52 @@ class TextState:
 
     def put_char(self, char):
         if self.cursor_pos >= len(self.current_line):
+            self.useless_keys += 1
             return
         if self.current_line[self.cursor_pos] == char:
+            if self.current_line[self.cursor_pos] ==\
+                    self.current_input[self.cursor_pos]:
+                self.useless_keys += 1
+            else:
+                self.useful_keys += 1
             if self.current_highlight[self.cursor_pos] == '_':
                 self.current_highlight[self.cursor_pos] = '+'
             else:
                 self.current_highlight[self.cursor_pos] = 'v'
         else:
             self.current_highlight[self.cursor_pos] = '-'
+            self.useless_keys += 1
         self.current_input[self.cursor_pos] = char
         self.cursor_pos += 1
         if self.input_len < self.cursor_pos:
             self.input_len = self.cursor_pos
 
     def new_line(self):
-        self.prev_line = self.current_line
+        self.useful_keys += 1
+        self.prev_line = self.current_line + ' ' + str(round(self.input_speed)) + 'spm '
+        self.prev_line += str(round(len(self.current_line) / (self.cycle_count / 600))) + 'spm'
         self.current_line = self.next_line
         self.next_line = self._str_gen.gen_rand_string(LINE_LEN)
         self.current_highlight = ['_'] * len(self.current_line)
         self.current_input = [''] * len(self.current_line)
         self.cursor_pos = 0
         self.input_len = 0
+        self.cycle_count = 0
 
     def move_left(self):
+        self.useless_keys += 1
         if self.cursor_pos > 0:
             self.cursor_pos -= 1
 
     def move_right(self):
+        self.useless_keys += 1
         if self.cursor_pos < self.input_len:
             self.cursor_pos += 1
+
+    def update_timer(self):
+        self.input_speed += 0.1*((self.useful_keys - self.prev_count)*600 - self.input_speed)
+        self.prev_count = self.useful_keys
+        self.cycle_count += 1
 
 
 class MainWindow:
@@ -93,7 +117,8 @@ class MainWindow:
         self.cvs.bind('<Key>', self.cvs_key_event)
         self.cvs.focus_set()
 
-        self.update()
+        self.timer()
+        #self.update()
 
     def update(self):
         # Clear canvas
@@ -101,7 +126,7 @@ class MainWindow:
         # Draw next line
         self.cvs.create_text(IDENT, IDENT,
                              anchor='nw',
-                             text=self.text_state.next_line, font='Consolas')
+                             text=self.text_state.next_line, font=FONT)
         # Draw current line with current input and highlight
         last_x = IDENT
         for i in range(len(self.text_state.current_line)):
@@ -113,7 +138,7 @@ class MainWindow:
             new_char_id = self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
                                                anchor='nw',
                                                text=new_char,
-                                               font='Consolas', fill=color)
+                                               font=FONT, fill=color)
             x1, y1, x2, y2 = self.cvs.bbox(new_char_id)
             if self.text_state.cursor_pos == i:
                 self.cvs.create_line(x1 + 1, y1 + 1, x1 + 1, y2 - 1, width=2)
@@ -126,8 +151,16 @@ class MainWindow:
                 self.cvs.create_text(last_x, IDENT + LINE_HEIGHT,
                                      anchor='nw',
                                      text=self.text_state.current_input[i],
-                                     font='Consolas',
-                                     fill='gray')
+                                     font=FONT,
+                                     fill='black')
+                self.cvs.create_rectangle(x1 + 1, y1 + 1 - LINE_HEIGHT * 2/3,
+                                          x2 - 1, y2 - 1 - LINE_HEIGHT * 2/3,
+                                          outline='', fill='lightblue', width='2p')
+                self.cvs.create_text(last_x, IDENT + LINE_HEIGHT * 1/3,
+                                     anchor='nw',
+                                     text=self.text_state.current_line[i],
+                                     font=FONT,
+                                     fill='black')
             elif highlight == 'x':  # Deleted
                 self.cvs.create_line(x1, y2, x2, y2, fill='red', width='2p')
             elif highlight == 'v':  # Fixed
@@ -136,7 +169,13 @@ class MainWindow:
         # Draw prev line
         self.cvs.create_text(IDENT, IDENT + 2*LINE_HEIGHT,
                              anchor='nw',
-                             text=self.text_state.prev_line, font='Consolas')
+                             text=self.text_state.prev_line, font=FONT)
+        stat = 'Keys statistics: useful: ' + str(self.text_state.useful_keys) +\
+               ' useless: ' + str(self.text_state.useless_keys) +\
+               ' speed: ' + str(round(self.text_state.input_speed)) + 'spm'
+        self.cvs.create_text(IDENT, IDENT + 3 * LINE_HEIGHT,
+                             anchor='nw',
+                             text=stat, font=FONT, fill='green')
 
     def cvs_key_event(self, event):
         if event.keysym == 'BackSpace':
@@ -154,6 +193,10 @@ class MainWindow:
             self.text_state.put_char(event.char)
         self.update()
 
+    def timer(self):
+        self.text_state.update_timer()
+        self.update()
+        self.window.after(100, self.timer)
 
 def main():
     main_window = MainWindow()
